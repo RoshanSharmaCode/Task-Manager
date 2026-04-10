cff8bbc4b0c0142a8266cd25e8d692209eac1178
Roshan <roshansharma9379>
2026-04-10 12:38:47 +0530

    Initial commit - Task Manager

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routers/task')
+const connectDB = require('./db/connection')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandler)
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening to port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
